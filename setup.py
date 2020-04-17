eac3208d39318a5647c1e962254943a17a24eb8d
Alvaro del Castillo <alvarodelcastillo@gmail.com>
2020-04-17 08:17:33 +0200

    Release v0.3.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='mcthings',
-    version='0.3.2',
+    version='0.3.3',
     packages=['mcthings'],
     include_package_data=True,
     license='ASL',
