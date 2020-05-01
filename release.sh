521780b60766d9eb548a385af84866344c8c2c9f
Alvaro del Castillo <alvarodelcastillo@gmail.com>
2020-05-02 01:07:50 +0200

    Add tests before uploading the release

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@
 # Commit and tag with the new version
 # Push branch with tags
 # Generate pip packages and upload to pip repository
+tests/run_tests.py
 rm dist/*
 python setup.py sdist
 python setup.py bdist_wheel
