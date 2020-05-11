1a58ce6913aaa1e4224842d5a852efd39b2a7721
Alvaro del Castillo <alvarodelcastillo@gmail.com>
2020-05-11 22:41:36 +0200

    Execute tests inside tests folder to find data files

--- release.sh
+++ release.sh
@@ -7,7 +7,9 @@
 # Commit and tag with the new version
 # Push branch with tags
 # Generate pip packages and upload to pip repository
-tests/run_tests.py
+cd tests
+./run_tests.py
+cd ..
 rm dist/*
 python setup.py sdist
 python setup.py bdist_wheel
