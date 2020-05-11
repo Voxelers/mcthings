e6024d2651d78d0470ddaf9428f4859084192471
Alvaro del Castillo <alvarodelcastillo@gmail.com>
2020-05-11 22:36:01 +0200

    Add Schematic support Thing

---
+++ tests/schematics/README.md
@@ -0,0 +1,8 @@
+# Schematics
+
+The schematics in this folder are samples used to develop the support
+of Schematic loading in McThings. Please, read the authors requirements
+for reusing them in your own creations:
+
+* [Pirate Boat](https://www.minecraft-schematics.com/schematic/68/)
+* [Chateau Fairmont](https://www.minecraft-schematics.com/schematic/9676/)
---
+++ tests/test_schematic.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Licensed under the terms of http://www.apache.org/licenses/LICENSE-2.0
+# Author (©): Alvaro del Castillo
+
+import logging
+import unittest
+
+from mcthings.schematic import Schematic
+from tests.base import TestBaseThing
+
+
+class TestSchematic(TestBaseThing):
+    """Test Schematic Thing"""
+
+    def test_build(self):
+
+        self.server.mc.postToChat("Building a schematic")
+        pos = self.pos
+
+        schematic = Schematic(pos)
+        # 2012: https://www.minecraft-schematics.com/schematic/68/
+        schematic.file_path = "schematics/pirate-boat.schematic"
+        # 2017: https://www.minecraft-schematics.com/schematic/9676/
+        # schematic.file_path = "schematics/chateau-fairmont.schematic"
+        # schematic.file_path = "schematics/palace-of-khalduun.schematic"
+        schematic.build()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    unittest.main(warnings='ignore')
